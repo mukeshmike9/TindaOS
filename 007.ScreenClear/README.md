e2eeb18a913626309a169545b494fc29a8e73a79
Mukesh Kumar Chaurasiya <mukeshmike9@gmail.com>
2019-07-12 01:07:12 +0530

    Screen Clear Init

---
+++ 007.ScreenClear/README.md
@@ -0,0 +1,1 @@
+# Writing byte data to hardware port
---
+++ 007.ScreenClear/kernel/libc/include/types.h
@@ -0,0 +1,10 @@
+#ifndef _TYPES_H
+#define _TYPES_H
+
+typedef unsigned int size_t;
+
+typedef unsigned char uint8_t;
+typedef unsigned short int uint16_t;
+typedef unsigned int uint32_t;
+
+#endif
